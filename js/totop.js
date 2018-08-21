850174a1484467e88400f2b336e14f50815b4ebc
振国 <jinzhenguo@jd.com>
2018-08-21 14:57:26 +0800

    Site updated: 2018-08-21 14:57:24

---
+++ js/totop.js
@@ -0,0 +1,3 @@
+// build time:Tue Aug 21 2018 14:52:03 GMT+0800 (China Standard Time)
+(function(o){var e=1e3;var t=o("#totop");var l=500;t.hide();o(window).scroll(function(){var l=o(document).scrollTop();if(l>e){o(t).stop().fadeTo(300,1)}else{o(t).stop().fadeTo(300,0)}});o(t).click(function(){o("html, body").animate({scrollTop:0},l);return false})})(jQuery);
+//rebuild by neat 
