45921c78c71da1892228bb641d9f6c43fbd047ce
jingle1267 <jingle1267@163.com>
2019-12-20 09:59:37 +0800

    Site updated: 2019-12-20 09:59:36

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Thu Dec 19 2019 12:07:22 GMT+0800 (China Standard Time)
+// build time:Fri Dec 20 2019 09:59:26 GMT+0800 (China Standard Time)
 (function(o){var e=1e3;var t=o("#totop");var l=500;t.hide();o(window).scroll(function(){var l=o(document).scrollTop();if(l>e){o(t).stop().fadeTo(300,1)}else{o(t).stop().fadeTo(300,0)}});o(t).click(function(){o("html, body").animate({scrollTop:0},l);return false})})(jQuery);
 //rebuild by neat 
